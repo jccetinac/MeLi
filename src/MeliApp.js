0aa212ec7529e7c35f44af923813c22c95a5c747
jccetinac <editorialdartagnan@iMac-de-EDITORIAL.local>
2021-03-24 08:29:52 -0500

    setting sass

--- src/MeliApp.js
+++ src/MeliApp.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { AppRouter } from './routers/AppRouter'
+import React from 'react';
+import { AppRouter } from './routers/AppRouter';
+import  "./styles/App.scss";
 
 export const MeLiApp = () => {
     return (
