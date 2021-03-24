fb030dd46b0800935aa877efb382ab9bc6135dc3
jccetinac <editorialdartagnan@iMac-de-EDITORIAL.local>
2021-03-24 17:40:52 -0500

    build app

---
+++ Docs/precache-manifest.f4b8f07164951594bc41f68e8c5f8613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fcfbd73d88c3e56bd39f307a83fe8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a226186f4b53babb981",
+    "url": "/static/css/main.75299e43.chunk.css"
+  },
+  {
+    "revision": "1dd5fd1d36b4a11915cd",
+    "url": "/static/js/2.8e266f9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e266f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a226186f4b53babb981",
+    "url": "/static/js/main.4cf63623.chunk.js"
+  },
+  {
+    "revision": "e08f888d9a08f8c84965",
+    "url": "/static/js/runtime-main.f2bb1637.js"
+  }
+]);
