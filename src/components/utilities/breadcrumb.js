b1ae13ea8c5cf3af6c78dd178b07578f9e6453e7
jccetinac <jccetinac@gmail.com>
2021-04-03 07:49:06 -0500

    props key bradcrumb

--- src/components/utilities/breadcrumb.js
+++ src/components/utilities/breadcrumb.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const BreadCrumbs = (categories) => {
     if(categories !== null){
-    return (<div className="badge  mb-3 paragraph"  > { categories.categories.map(cat => (<span key={cat} > | { cat } </span>))} </div>)
+    return (<div className="badge  mb-3 paragraph" key="breadcrumbsList" > { categories.categories.map(cat => (<span key={cat} > | { cat } </span>))} </div>)
     }
     else {
         return null
